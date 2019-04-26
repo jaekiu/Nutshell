89d74782e9b56b2f49f2f8e67859714c2a30bfdf
Shaurya Sanghvi <shaurya_s@berkeley.edu>
2019-04-26 14:16:40 -0700

    the world cant see me

--- app/src/main/java/com/jackie/nutshell/Proj.java
+++ app/src/main/java/com/jackie/nutshell/Proj.java
@@ -5,14 +5,33 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Proj {
-    private TextView mProjName;
-    private TextView mProjDes;
-    private ImageView mProfile;
-    private Button viewbutton;
-    private Button mApply;
+    public String _name;
+    public String _desc;
+    public String[] _skills;
+    public String _poster;
 
-    public Proj() {
 
+    public Proj(String name, String desc, String[] skills, String poster) {
+        _name = name;
+        _desc = desc;
+        _skills = skills;
+        _poster = poster;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String getDesc() {
+        return _desc;
+    }
+
+    public String[] getSkills() {
+        return _skills;
+    }
+
+    public String getPoster() {
+        return _poster;
     }
 
 }
